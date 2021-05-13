5303603aa8f254e59e88fe6f4d7c2ee80106cb1d
Alex <alexikam@gmail.com>
2021-05-13 14:22:02 +0300

    3

--- test/server/script-processor-test.js
+++ test/server/script-processor-test.js
@@ -1189,7 +1189,7 @@ describe('Script processor', () => {
 
                     expected: 'for (let _hh$temp0 of q) { let b = _hh$temp0[0], _hh$temp1 = _hh$temp0[1]; console.log(a); let a = 1;}'
                 },
-                // NOTE: we should rename the `for-of left` var only if we redeclare it on the root level
+                // NOTE: we should not rename the `for-of left` var if it is redeclared in the deeper statement
                 {
                     src: 'for (let [a] of q) { if (true) { let a = 1; } }',
 
